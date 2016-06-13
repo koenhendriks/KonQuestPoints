7707d34e20f605e9e63bb6301b1ba939e679fe3f
Koen Hendriks <info@koenhendriks.com>
2016-06-13 22:07:32 +0200

    removed anti ban GUI

--- src/konquestpoints/Gui.java
+++ src/konquestpoints/Gui.java
@@ -25,10 +25,10 @@ import java.awt.*;
         gr.drawString("Konbot Quest Points", 15, 311);
         gr.setFont(new Font("Arial", Font.PLAIN, 11));
         gr.drawString("Runtime : " + runTimer.getTimeRunningString(), 350, 311);
-        gr.drawString("Antiban lookaround : " + AntiBan.lookAroundTimer.getRemaining(), 0, 15);
-        gr.drawString("Antiban running : " + AntiBan.runningTimer.getRemaining(), 300, 15);
-        gr.drawString("Antiban tabs : " + AntiBan.tabsTimer.getRemaining(), 0, 28);
-        gr.drawString("Antiban mouse : " + AntiBan.mouseTimer.getRemaining(), 300, 28);
+//        gr.drawString("Antiban lookaround : " + AntiBan.lookAroundTimer.getRemaining(), 0, 15);
+//        gr.drawString("Antiban running : " + AntiBan.runningTimer.getRemaining(), 300, 15);
+//        gr.drawString("Antiban tabs : " + AntiBan.tabsTimer.getRemaining(), 0, 28);
+//        gr.drawString("Antiban mouse : " + AntiBan.mouseTimer.getRemaining(), 300, 28);
         gr.drawString("Current Quest : " + Quest.getActiveQuest(), 15, 330);
         gr.drawString("Doing : " + Quest.getAction(), 270, 330);
 
